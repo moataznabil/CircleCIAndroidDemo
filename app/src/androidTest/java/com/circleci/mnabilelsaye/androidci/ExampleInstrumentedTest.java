ef4967148c82fcc48593369d40128f4ac4125779
mnabilelsaye <moataz.nabil@zalando-lounge.de>
2017-12-20 16:44:04 +0100

    repair package name

--- app/src/androidTest/java/com/circleci/mnabilelsaye/androidci/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/circleci/mnabilelsaye/androidci/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.circleci.moataznabil", appContext.getPackageName());
+        assertEquals("io.circleci.moataznabil.androidci", appContext.getPackageName());
     }
 }
